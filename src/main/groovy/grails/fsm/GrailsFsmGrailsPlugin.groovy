6db7df68122f5b0f78815ff432f9fad7996f1dda
Doug Beasley <beasley.doug@gmail.com>
2016-05-10 17:33:13 -0700

    fixing plugin class

--- src/main/groovy/grails/fsm/GrailsFsmGrailsPlugin.groovy
+++ src/main/groovy/grails/fsm/GrailsFsmGrailsPlugin.groovy
@@ -1,22 +1,49 @@
 package grails.fsm
 
 import grails.plugins.*
+import groovy.lang.MetaProperty;
+import org.grails.spring.DefaultBeanConfiguration;
+
+import org.codehaus.groovy.runtime.metaclass.ThreadManagedMetaBeanProperty;
+
+import grails.plugin.fsm.FsmSupport
+import grails.plugin.fsm.FsmUtils
+import grails.plugin.fsm.FsmSupportException
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.context.ApplicationContext;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import grails.util.GrailsClassUtils
+import org.grails.commons.metaclass.*
 
 class GrailsFsmGrailsPlugin extends Plugin {
-
+    private static final Log log = LogFactory.getLog(GrailsFsmGrailsPlugin.class);
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "3.1.4 > *"
+
+    def loadAfter = ['controllers', 'hibernate', 'domainclass']
+
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-        "grails-app/views/error.gsp"
+        "grails-app/views/error.gsp",
+        "FsmSupportDummy.groovy",
+        "FsmMultipleActions.groovy",
+        "Foo.groovy",
+        "Foo2.groovy"
     ]
 
     // TODO Fill in these fields
-    def title = "Grails Fsm" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
-    def description = '''\
-Brief summary/description of the plugin.
+    def title = "Finite State Machine behaviour for domain classes"
+    def author = "Jorge Uriarte"
+    def authorEmail = "jorge.uriarte@omelas.net"
+    def description = '''\\
+        This plugin allow definition of simple workflows attached to domain classes, including
+        states, events, transitions and conditions.
+        Current workflow's state will be held in domain class' property that must be defined.
+        Multiple workflows can be defined on every domain class.
 '''
     def profiles = ['web']
 
@@ -46,17 +73,69 @@ Brief summary/description of the plugin.
     }
 
     void doWithDynamicMethods() {
-        // TODO Implement registering dynamic methods to classes (optional)
+
+        // Will add the fire closure where needed
+        grailsApplication.domainClasses.each {domainClass ->
+            MetaClassRegistry registry = GroovySystem.metaClassRegistry
+            def fsm = GrailsClassUtils.getStaticPropertyValue(domainClass.clazz, FsmUtils.FSMDEF )
+            if (fsm) {
+                // Will create the proper FsmSupport instance!
+                fsm.each {String p, definition ->
+                    definition.each { start, defclosure ->
+                      def mp = domainClass.metaClass.getMetaProperty(p)
+                        if (!mp)
+                            throw new FsmSupportException("Error in FSM definition: '${domainClass.clazz}' does not have '${p}' property to hold defined workflow status!")
+                      def tmp = domainClass.clazz.newInstance()
+                      if (tmp[p] != null)
+                        log.warn("Default value of '${domainClass.clazz}.${p}' will be overriden by FSM definition for that property. ")
+
+                        // Modify the metaclass so new instances will have new behaviour!!
+                        domainClass.metaClass.setProperty("_fsm${p}", null)  // internal, will hold FsmSupport instance
+                        domainClass.metaClass.fire = FsmUtils.fireClosure
+                        domainClass.metaClass."fire_${p}" = FsmUtils.fireClosure.curry(p)
+                        domainClass.metaClass.fireable = FsmUtils.fireableClosure
+                        domainClass.metaClass."fireable_${p}" = FsmUtils.fireableClosure.curry(p)
+                    }
+                }
+                // This code is a COPY of DomainClassGrailsPlugin.enhanceDomainClasses
+                // because I cannot seem to be able to decorate it.
+                // We just added the "${p}" initializing!
+                domainClass.metaClass.constructor = {->
+                def bean
+                  if(applicationContext.containsBean(domainClass.fullName)) {
+                      bean = applicationContext.getBean(domainClass.fullName)
+                  }
+                  else {
+                      bean = BeanUtils.instantiateClass(domainClass.clazz)
+                  }
+                fsm.each { pp, defdef ->
+                  defdef.each { startstart, clos ->
+    //	            			def setter = GrailsClassUtils.getSetterName(pp)
+    //	            			bean."${setter}"(startstart)
+                    bean."${pp}" = startstart
+                  }
+                }
+                bean
+              }
+                domainClass.metaClass.static.create = {->
+                  def bean = applicationContext.getBean(domainClass.getFullName())
+                fsm.each { pp, defdef ->
+                  defdef.each { startstart, clos ->
+                    bean."${pp}" = startstart
+                  }
+                }
+                  bean
+                }
+            }
+        }
     }
 
     void doWithApplicationContext() {
         // TODO Implement post initialization spring config (optional)
     }
 
-    void onChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
+    void onChange( Map<String, Object> event) {
+    	event.manager?.getGrailsPlugin("fsm")?.doWithDynamicMethods(event.ctx)
     }
 
     void onConfigChange(Map<String, Object> event) {
